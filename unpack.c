c78fd59340818a532330b6d7787f7505982573bc
Ben Baker <benbaker@isti.com>
2016-06-02 07:50:58 -0700

    Fixed some preamble comments

--- unpack.c
+++ unpack.c
@@ -20,12 +20,15 @@ float unpack_float(char *s, bool lswap)
 {
     char s4[4];
     float f4;
-    if (lswap){
+    if (lswap)
+    {
         s4[0] = s[3];
         s4[1] = s[2];
         s4[2] = s[1];
         s4[3] = s[0];
-    }else{
+    }
+    else
+    {
         s4[0] = s[0];
         s4[1] = s[1];
         s4[2] = s[2];
@@ -36,13 +39,13 @@ float unpack_float(char *s, bool lswap)
 }
 //============================================================================//
 /*!
- * @brief Converts a char from a binary file into a floating value
+ * @brief Converts a char from a binary file into a double value
  *
  * @param[in] s      character string to unpack
  * @param[in] lswap  =True -> then swap bytes
  *                   =False -> then do not swap bytes (default)
  *
- * @result floating value corresponding to s from binary file
+ * @result double value corresponding to s from binary file
  *
  * @author Ben Baker, ISTI
  *
@@ -51,7 +54,8 @@ double unpack_double(char *s, bool lswap)
 {
     char s8[8];
     double f8; 
-    if (lswap){
+    if (lswap)
+    {
         s8[0] = s[7];
         s8[1] = s[6];
         s8[2] = s[5];
@@ -60,7 +64,9 @@ double unpack_double(char *s, bool lswap)
         s8[5] = s[2];
         s8[6] = s[1];
         s8[7] = s[0];
-    }else{
+    }
+    else
+    {
         s8[0] = s[0];
         s8[1] = s[1];
         s8[2] = s[2];
